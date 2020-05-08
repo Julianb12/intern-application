f5096c3ef9174b3b1c245e887be41c22d5e53813
Julianb12 <julianb1212@gmail.com>
2020-05-08 17:30:05 -0500

    initial changes

--- src/Child.js
+++ src/Child.js
@@ -1,5 +1,39 @@
-function Child() {
-  return null
+import React from 'react';
+import './Child.css'
+
+function Child(props) {
+
+  function handleColorChange() {
+    return (
+      props.setColor('#A52A2A')
+    )
+  }
+  
+  function handlePlacementChange() {
+    
+  }
+ 
+  if (props.blink === true){
+    return (
+      <div>
+        <button onClick={handleColorChange}>
+          change color
+        </button>
+  
+      </div>
+    )
+  }else{
+    return (
+      <div>
+        <button onClick={handleColorChange}>
+          change placement
+        </button>
+  
+      </div>
+    )
+  }
+
+   
 }
 
 export default Child
